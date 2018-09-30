ccaf15fe12f625bea9fac29a31a4af8d025823a0
pingli.zheng <pingli.zheng@ucarinc.com>
2018-09-30 15:52:43 +0800

    过滤掉所有页面标签

--- src/components/Rightbar.vue
+++ src/components/Rightbar.vue
@@ -160,8 +160,6 @@ export default {
     },
     filterHTMLTag(msg) {
       var msg = msg.replace(/<\/?[^>]*>/g, '') // 去除HTML Tag
-      msg = msg.replace(/[|]*\n/, '') // 去除行尾空格
-      msg = msg.replace(/&npsp;/ig, '') // 去掉npsp
       return msg
     }
   }
