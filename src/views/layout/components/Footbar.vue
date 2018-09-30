e77e30c9a7ff032ec02f63e7b8629008a4a5c0c5
pingli.zheng <pingli.zheng@ucarinc.com>
2018-09-30 15:00:01 +0800

    修改页面底部

--- src/views/layout/components/Footbar.vue
+++ src/views/layout/components/Footbar.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="fly-footer">
     <p>
-      <a href="#" target="_blank">神州优车社区</a> 2017 &copy;
-      <a href="#" target="_blank">ucar.com 出品</a>
+    <!--  <a href="#" target="_blank">神州优车社区</a>-->
+      <a href="#" target="_blank">Copyright2018 &copy; w3.ucarinc.com All Rights Reserved</a>
     </p>
     <p>
-      <a href="#" target="_blank">联系管理员：qq@qq.com</a>
+      <a href="#" target="_blank">神州优车集团内部论坛</a>
     </p>
   </div>
 </template>
